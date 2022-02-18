2fcbc3750065477b6ea1c8c41198fdc0f3bb6da4
Cristian Padilla <Cristian.PadillaJimenez@telus.com>
2022-02-18 16:59:08 -0500

    Initializing repository for DSU 2022 v1

---
+++ src/main/java/com/telus/ds/test/DsMyTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.telus.ds.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMyTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMyTrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telus/ds/test/DsMyTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telus.ds.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMyTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
